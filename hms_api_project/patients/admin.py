011f78fe3d31db994e7c9487c2a662a24c08cec4
Alle4n <allennajjuma96@gmail.com>
2026-01-04 10:02:50 +0300

    feat(admin): register Patient model in admin panel

--- hms_api_project/patients/admin.py
+++ hms_api_project/patients/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "medical_record_number",
+        "gender",
+        "date_of_birth",
+        "contact_phone",
+        "created_at",
+        "is_deleted",
+    )
+    list_filter = ("gender", "is_deleted", "created_at")
+    search_fields = (
+        "first_name",
+        "last_name",
+        "medical_record_number",
+        "contact_phone",
+        "contact_email",
+    )
+    ordering = ("-created_at",)
