96fed5cd421f23e5b2c3f3eca82c056b3fdc277d
Alle4n <allennajjuma96@gmail.com>
2026-01-04 12:21:10 +0300

    feat(admin): register Attachment model in admin panel

--- hms_api_project/attachments/admin.py
+++ hms_api_project/attachments/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Attachment
 
-# Register your models here.
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "record",
+        "file",
+        "uploaded_by",
+        "uploaded_at",
+    )
+    search_fields = (
+        "record__patient__first_name",
+        "record__patient__last_name",
+        "record__doctor__first_name",
+        "record__doctor__last_name",
+        "file",
+    )
+    list_filter = ("uploaded_at", "uploaded_by")
+    ordering = ("-uploaded_at",)
