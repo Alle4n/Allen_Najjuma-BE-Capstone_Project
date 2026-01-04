8d7f1e9740c1de8f56fe3af172ec5e882d232df5
Alle4n <allennajjuma96@gmail.com>
2026-01-04 11:27:15 +0300

    feat(admin): register Doctor model in admin panel

--- hms_api_project/doctors/admin.py
+++ hms_api_project/doctors/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Doctor
 
-# Register your models here.
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "specialty",
+        "license_number",
+        "contact_phone",
+        "created_at",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "specialty",
+        "license_number",
+        "contact_phone",
+    )
+    ordering = ("last_name", "first_name")
